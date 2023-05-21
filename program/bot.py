33c1d7e6fb145bf10f89cf818c2d0051176483ae
Heng Tse Chou <legohank1219@gmail.com>
2023-05-21 15:34:22 +0800

    feat: new entries

--- program/bot.py
+++ program/bot.py
@@ -9,6 +9,7 @@ import random
 import logging
 
 from program.config import CHANNEL_ACCESS_TOKEN, CHANNEL_SECRET, HANK_ID, LALA_ID
+from program.db import Mongo
 
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
@@ -18,8 +19,8 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
 # add random response
-
 resp = ['哈哈 真假', '0.0', '7414', '喵喵喵']
+
 
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
@@ -41,185 +42,228 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-def receive_message_and_edit_file(event):    
+def receive_message(event):    
 
     # main feature of split money linebot
     # hank and lala only
     if event.source.user_id == LALA_ID or event.source.user_id == HANK_ID:
 
-        if 'hank' in event.message.text or 'Hank' in event.message.text:
-            message = event.message.text
-            amount = message.split(' ')[-1]
+        if '結算' in event.message.text or '結清' in event.message.text or '算帳' in event.message.text:
+            pass
 
-            if amount.isdigit():
-                try:
-                    drive = drive_method('ledger.csv', config.get('drive-api', 'file_id'))
-                    new_entry(drive, 'hank', amount)
-
-                    line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text='Hank 付了 ' + str(amount) + '\n登記好了!'))
-                    app.logger.info('New record entered.')
-                except ApiRequestError as e:
-                    line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text='連線異常! 發呆!!\n\n' + str(e)))
-                    app.logger.error('Drive server error.')
-                except Exception as e:
-                    line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text='我怪怪的!!\n\n' + str(e)))
-                    app.logger.error('Some error.\n' + str(e))
-            else:
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='格式不對 要重新輸入!'))
-
-            return
-
-
-        elif 'lala' in event.message.text or 'Lala' in event.message.text:
-            message = event.message.text
-            amount = message.split(' ')[-1]
-
-            if amount.isdigit():
-                try:
-                    drive = drive_method('ledger.csv', config.get('drive-api', 'file_id'))
-                    new_entry(drive, 'lala', amount)
-
-                    line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text='Lala 付了 ' + str(amount) + '\n登記好了!'))
-                    app.logger.info('New record entered.')
-                except ApiRequestError as e:
-                    line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text='連線異常! 發呆!!\n\n' + str(e)))
-                    app.logger.error('Drive server error.')
-                except Exception as e:
-                    line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text='我怪怪的!!\n\n' + str(e)))
-                    app.logger.error('Some error.\n' + str(e))
-            else:
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='格式不對 要重新輸入!'))
-            return
-
-
-        elif '結算' in event.message.text or '結清' in event.message.text or '算帳' in event.message.text:
-
-            drive = drive_method('ledger.csv', config.get('drive-api', 'file_id'))
-            drive.download()            
-            df = pd.read_csv('ledger.csv')
-
-            hank_minus_lala = df.hank.sum() - df.lala.sum()
-
-            if hank_minus_lala > 0:
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='Lala 要給 Hank ' + str(hank_minus_lala) + '元!\n帳目已結清'))
-
-            elif hank_minus_lala < 0:
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='Hank 要給 Lala ' + str(hank_minus_lala*-1) + '元!\n帳目已結清'))
-
-            elif hank_minus_lala == 0:
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='剛好花一樣錢!! amazing!!'))
-
-            df = df[0:0]
-            df.to_csv('ledger.csv', index=False)
-            drive.upload()
-            app.logger.info('Ledger settled.')
-            return
+        elif '試算' in event.message.text or '算一下' in event.message.text:
+            pass
 
         elif '偷看一下' in event.message.text:
-            drive = drive_method('ledger.csv', config.get('drive-api', 'file_id'))
-            drive.download()
-            df = pd.read_csv('ledger.csv')
-            if df.empty:
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text='現在是空的!'))
-            else:
-                text = df.to_string(index=False).split('\n')
-                nlines = len(text)
-                segment_text = ''
-                for i in range(nlines):
-                    segment_text += text[i]
-                    segment_text += '\n'
-                    if i % 10 == 0 and i != 0:
-                        segment_text += '-----------------\n'
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text='偷看一下! \n-----------------\n' + segment_text.strip('\n')))
-            app.logger.info('Ledger screenshot. ')
-            return
+            pass
 
         elif '功能表' in event.message.text or '指令表' in event.message.text:
             line_bot_api.reply_message(
                 event.reply_token, 
-                TextSendMessage(text='記帳:\nLala or Hank 空一格 金額\n----------\n其他功能:\n目前帳目->偷看一下\n試算金額->算一下/試算\n導覽頁面->指令表/功能表')
+                TextSendMessage(text='記帳:\nLala or Hank 項目 金額\n----------\n其他功能:\n目前帳目->偷看一下\n試算金額->算一下/試算\n導覽頁面->指令表/功能表')
             )
             app.logger.info('Show menu')
-            return
+            return       
 
-        elif '試算' in event.message.text or '算一下' in event.message.text:
+        elif 'hank' in event.message.text or 'Hank' in event.message.text or 'lala' in event.message.text or 'Lala' in event.message.text: 
 
-            drive = drive_method('ledger.csv', config.get('drive-api', 'file_id'))
-            drive.download()            
-            df = pd.read_csv('ledger.csv')
+            message = event.message.text.split(' ')
+            payer = message[0].lower()
+            item = message[1]
+            amount = message[2]
 
-            hank_minus_lala = df.hank.sum() - df.lala.sum()
+            if amount.isdigit():
 
-            if hank_minus_lala > 0:
+                Mongo.insert_new(payer, item, amount)
                 line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='Lala 現在欠 Hank ' + str(hank_minus_lala) + '元喔!'))
+                    event.reply_token,
+                    TextSendMessage(text=''.join([payer, ' 付了 ', amount, '\n登記好了!'])))
+                app.logger.info('New record entered.')
 
-            elif hank_minus_lala < 0:
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='Hank 現在欠 Lala ' + str(hank_minus_lala*-1) + '元喔!'))
-
-            elif hank_minus_lala == 0:
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='現在剛好花一樣錢喔!'))
-            app.logger.info('Show current amount.')
-            return
-
-    if event.source.user_id == LALA_ID:
-        if '寶寶' in event.message.text:
-            if event.source.type == 'group':
-                id = event.source.group_id
             else:
-                id = LALA_ID
-            line_bot_api.push_message(
-            id,
-            TextSendMessage(text='是喔'))
-            time.sleep(0.5)
-            line_bot_api.push_message(
-            id,
-            TextSendMessage(text='澳草喔'))
-            time.sleep(0.5)
-            line_bot_api.push_message(
-            id,
-            TextSendMessage(text='愛你喔'))
-            time.sleep(0.5)
-            line_bot_api.push_message(
-            id,
-            TextSendMessage(text='寶寶')) 
-            return
+                line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='格式不對 要重新輸入!'))
 
-    num = random.randint(0, len(resp)-1)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=resp[num]))
+        else:
+
+            if event.source.user_id == LALA_ID:
+                if '寶寶' in event.message.text:
+                    if event.source.type == 'group':
+                        id = event.source.group_id
+                    else:
+                        id = LALA_ID
+                    line_bot_api.push_message(
+                    id,
+                    TextSendMessage(text='是喔'))
+                    time.sleep(0.5)
+                    line_bot_api.push_message(
+                    id,
+                    TextSendMessage(text='澳草喔'))
+                    time.sleep(0.5)
+                    line_bot_api.push_message(
+                    id,
+                    TextSendMessage(text='愛你喔'))
+                    time.sleep(0.5)
+                    line_bot_api.push_message(
+                    id,
+                    TextSendMessage(text='寶寶')) 
+                    return
+
+            num = random.randint(0, len(resp)-1)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=resp[num]))
+
+
+
+        # if 'hank' in event.message.text or 'Hank' in event.message.text:
+            
+        #     amount = message.split(' ')[-1]
+
+        #     if amount.isdigit():
+        #         try:
+        #             drive = drive_method('ledger.csv', config.get('drive-api', 'file_id'))
+        #             new_entry(drive, 'hank', amount)
+
+        #             line_bot_api.reply_message(
+        #             event.reply_token,
+        #             TextSendMessage(text='Hank 付了 ' + str(amount) + '\n登記好了!'))
+        #             app.logger.info('New record entered.')
+        #         except ApiRequestError as e:
+        #             line_bot_api.reply_message(
+        #             event.reply_token,
+        #             TextSendMessage(text='連線異常! 發呆!!\n\n' + str(e)))
+        #             app.logger.error('Drive server error.')
+        #         except Exception as e:
+        #             line_bot_api.reply_message(
+        #             event.reply_token,
+        #             TextSendMessage(text='我怪怪的!!\n\n' + str(e)))
+        #             app.logger.error('Some error.\n' + str(e))
+        #     else:
+        #         line_bot_api.reply_message(
+        #         event.reply_token,
+        #         TextSendMessage(text='格式不對 要重新輸入!'))
+
+        #     return
+
+
+        # elif 'lala' in event.message.text or 'Lala' in event.message.text:
+        #     message = event.message.text
+        #     amount = message.split(' ')[-1]
+
+        #     if amount.isdigit():
+        #         try:
+        #             drive = drive_method('ledger.csv', config.get('drive-api', 'file_id'))
+        #             new_entry(drive, 'lala', amount)
+
+        #             line_bot_api.reply_message(
+        #             event.reply_token,
+        #             TextSendMessage(text='Lala 付了 ' + str(amount) + '\n登記好了!'))
+        #             app.logger.info('New record entered.')
+        #         except ApiRequestError as e:
+        #             line_bot_api.reply_message(
+        #             event.reply_token,
+        #             TextSendMessage(text='連線異常! 發呆!!\n\n' + str(e)))
+        #             app.logger.error('Drive server error.')
+        #         except Exception as e:
+        #             line_bot_api.reply_message(
+        #             event.reply_token,
+        #             TextSendMessage(text='我怪怪的!!\n\n' + str(e)))
+        #             app.logger.error('Some error.\n' + str(e))
+        #     else:
+        #         line_bot_api.reply_message(
+        #         event.reply_token,
+        #         TextSendMessage(text='格式不對 要重新輸入!'))
+        #     return
+
+
+        # elif '結算' in event.message.text or '結清' in event.message.text or '算帳' in event.message.text:
+
+        #     drive = drive_method('ledger.csv', config.get('drive-api', 'file_id'))
+        #     drive.download()            
+        #     df = pd.read_csv('ledger.csv')
+
+        #     hank_minus_lala = df.hank.sum() - df.lala.sum()
+
+        #     if hank_minus_lala > 0:
+        #         line_bot_api.reply_message(
+        #         event.reply_token,
+        #         TextSendMessage(text='Lala 要給 Hank ' + str(hank_minus_lala) + '元!\n帳目已結清'))
+
+        #     elif hank_minus_lala < 0:
+        #         line_bot_api.reply_message(
+        #         event.reply_token,
+        #         TextSendMessage(text='Hank 要給 Lala ' + str(hank_minus_lala*-1) + '元!\n帳目已結清'))
+
+        #     elif hank_minus_lala == 0:
+        #         line_bot_api.reply_message(
+        #         event.reply_token,
+        #         TextSendMessage(text='剛好花一樣錢!! amazing!!'))
+
+        #     df = df[0:0]
+        #     df.to_csv('ledger.csv', index=False)
+        #     drive.upload()
+        #     app.logger.info('Ledger settled.')
+        #     return
+
+        # elif '偷看一下' in event.message.text:
+        #     drive = drive_method('ledger.csv', config.get('drive-api', 'file_id'))
+        #     drive.download()
+        #     df = pd.read_csv('ledger.csv')
+        #     if df.empty:
+        #         line_bot_api.reply_message(
+        #             event.reply_token,
+        #             TextSendMessage(text='現在是空的!'))
+        #     else:
+        #         text = df.to_string(index=False).split('\n')
+        #         nlines = len(text)
+        #         segment_text = ''
+        #         for i in range(nlines):
+        #             segment_text += text[i]
+        #             segment_text += '\n'
+        #             if i % 10 == 0 and i != 0:
+        #                 segment_text += '-----------------\n'
+        #         line_bot_api.reply_message(
+        #             event.reply_token,
+        #             TextSendMessage(text='偷看一下! \n-----------------\n' + segment_text.strip('\n')))
+        #     app.logger.info('Ledger screenshot. ')
+        #     return
+
+        # elif '功能表' in event.message.text or '指令表' in event.message.text:
+        #     line_bot_api.reply_message(
+        #         event.reply_token, 
+        #         TextSendMessage(text='記帳:\nLala or Hank 空一格 金額\n----------\n其他功能:\n目前帳目->偷看一下\n試算金額->算一下/試算\n導覽頁面->指令表/功能表')
+        #     )
+        #     app.logger.info('Show menu')
+        #     return
+
+        # elif '試算' in event.message.text or '算一下' in event.message.text:
+
+        #     drive = drive_method('ledger.csv', config.get('drive-api', 'file_id'))
+        #     drive.download()            
+        #     df = pd.read_csv('ledger.csv')
+
+        #     hank_minus_lala = df.hank.sum() - df.lala.sum()
+
+        #     if hank_minus_lala > 0:
+        #         line_bot_api.reply_message(
+        #         event.reply_token,
+        #         TextSendMessage(text='Lala 現在欠 Hank ' + str(hank_minus_lala) + '元喔!'))
+
+        #     elif hank_minus_lala < 0:
+        #         line_bot_api.reply_message(
+        #         event.reply_token,
+        #         TextSendMessage(text='Hank 現在欠 Lala ' + str(hank_minus_lala*-1) + '元喔!'))
+
+        #     elif hank_minus_lala == 0:
+        #         line_bot_api.reply_message(
+        #         event.reply_token,
+        #         TextSendMessage(text='現在剛好花一樣錢喔!'))
+        #     app.logger.info('Show current amount.')
+        #     return
+
+    
 
 
 
--- program/db.py
+++ program/db.py
@@ -7,13 +7,37 @@ class Mongo_object(object):
     def __init__(self):
 
         client = pymongo.MongoClient(MONGO_URL, connect=False)
-        db = client["map_bot"]
-        self.collection = db["users"]
+        db = client["money_bot"]
+        self.collection = db["records"]
 
-    def insert_new(self, payer, amount):
+    def insert_new(self, payer, item, amount):
 
         now_time_gmt_plus_8 = datetime.now() + timedelta(hours=8)
         date = now_time_gmt_plus_8.strftime("%m/%d")
-        new_insert = {'date':date, 'payer':payer, 'amount': amount}
+        new_insert = {'date':date, 'payer':payer, 'item': item, 'amount': amount}
         x = self.collection.insert_one(new_insert)
         return x
+    
+    def clear(self):
+
+        self.drop()
+    
+    def two_sum_difference(self):
+
+        pipeline1 = [
+            {'$match': {'payer': 'hank'}},
+            {'$group': {'_id': None, 'total': {'$sum': '$amount'}}}
+        ]
+
+        pipeline2 = [
+            {'$match': {'payer': 'lala'}},
+            {'$group': {'_id': None, 'total': {'$sum': '$amount'}}}
+        ]
+
+        return
+    
+    def print_records(self):
+        
+        return
+
+Mongo = Mongo_object()
