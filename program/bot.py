52b168cb035420f4e4d5ed3bf0cab488cb7f60f6
Heng Tse Chou <legohank1219@gmail.com>
2023-05-21 17:20:53 +0800

    fix: table formatting

--- program/bot.py
+++ program/bot.py
@@ -97,7 +97,7 @@ def receive_message(event):
             data = [dict(record) for record in records]
 
             if data:
-                table = tabulate(data, headers="keys", tablefmt="plain", stralign='right')
+                table = tabulate(data, headers="keys", tablefmt="plain")
                 line_bot_api.reply_message(
                     event.reply_token,
                     TextSendMessage(text='偷看一下! \n-----------------------\n' + table)
